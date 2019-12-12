040acc34e963992f67d45441076965e9ebb51da0
Daniel Matney <danieljmatney@gmail.com>
2019-12-12 03:00:35 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,9 +3,9 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.price :integer
-      t.size :integer
-      t.image_url :string
+      t.integer :price
+      t.integer :size
+      t.string :image_url
       t.timestamps
     end
   end
