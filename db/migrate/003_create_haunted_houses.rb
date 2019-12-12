d4acbd0b89f55f06ba122aeaf4279d1fe6e1dfe6
Daniel Matney <danieljmatney@gmail.com>
2019-12-12 03:03:00 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,14 +2,14 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
 
   def change
     create_table :haunted_houses do |h|
-    h.name :string
-    h.location :string
-    h.theme :string
-    h.price :float
-    h.family_friendly :boolean
-    h.opening_time :datetime
-    h.closing_time :datetime
-    h.long_description :text
+    h.string :name
+    h.string :location
+    h.string :theme
+    h.float :price
+    h.boolean :family_friendly
+    h.datetime :opening_time
+    h.datetime :closing_time
+    h.text :long_description
     end
   end
 end
