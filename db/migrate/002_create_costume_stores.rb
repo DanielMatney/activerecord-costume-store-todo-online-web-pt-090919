1ca38342327bf3497daae587659961f11768b50d
Daniel Matney <danieljmatney@gmail.com>
2019-12-12 02:56:39 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :costume_stores do |c|
+    c.string :name
+    c.location :string
+    c.costume_inventory :integer
+    c.number_of_employees :integer
+    c.in_business :boolean
+    c.opening_time :datetime
+    c.closing_time :datetime
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :haunted_houses do |h|
+    h.name :string
+    h.location :string
+    h.theme :string
+    h.price :float
+    h.family_friendly :boolean
+    h.opening_time :datetime
+    h.closing_time :datetime
+    h.long_description :text
+    end
+  end
+end
